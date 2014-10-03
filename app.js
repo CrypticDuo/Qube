da2264b681dd28e7a122f1d9a6329c34aaf0e017
Insoo Choo <earl93@hotmail.com>
2014-10-02 22:36:42 -0400

    ensure authenticated for all apis

--- app.js
+++ app.js
@@ -18,21 +18,19 @@ var app = express();
 app.set("view options", {
     layout: false
 });
-//app.use(bodyParser.urlencoded());
+
 app.use("/static", express.static(__dirname + "/static"));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('port', process.env.PORT || 4455);
-app.use(express.session({
-    secret: 'my_precious'
-}));
+app.use(express.session({secret: 'my_precious'}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({extended:true}));
 
 // serialize and deserialize
 passport.serializeUser(function(user, done) {
-    console.log('serializeUser: ' + user._id);
+    //console.log('serializeUser: ' + user._id);
     done(null, user._id);
 });
 passport.deserializeUser(function(id, done) {
@@ -43,7 +41,6 @@ passport.deserializeUser(function(id, done) {
 });
 
 // RENDERING
-
 app.get('/account', ensureAuthenticated, function(req, res) {
     User.findById(req.session.passport.user, function(err, user) {
         if (err) {
@@ -56,23 +53,21 @@ app.get('/account', ensureAuthenticated, function(req, res) {
     });
 });
 
-app.post('/addPlaylist', function(req, res){
+app.post('/addPlaylist', ensureAuthenticated, function(req, res){
     db.createPlaylist(req.user.oauthID, req.body.playlistName, function(result){
 		res.json(result);
 	});
 });
-app.get('/listAllPlaylist', function(req, res){
+app.get('/listAllPlaylist', ensureAuthenticated, function(req, res){
     db.listAllPlaylist(req.user.oauthID, function(result){
         res.json(result);
     });
 });
-app.post('/addVideoToPlaylist', function(req, res){
-    console.log(req.body);
+app.post('/addVideoToPlaylist', ensureAuthenticated, function(req, res){
     db.addVideoToPlaylist(req.user.oauthID, req.body.playlistName, req.body.videoID, function(result){
         res.json(result);
     });
 });
-
 app.get('/', function(req, res) {
     res.render('index.html', {
         user: req.user
