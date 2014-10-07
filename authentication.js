3c6c283ffbfa5a959f2641c5d506cfa4ed252230
Paul <bumj94@gmail.com>
2014-10-06 21:09:20 -0400

    save added facebook id

--- authentication.js
+++ authentication.js
@@ -24,6 +24,7 @@ module.exports = passport.use(new FacebookStrategy({
             } else {
                 var user = new User({
                     oauthID: profile.id,
+                    facebookID: profile._json.id,
                     name: profile.displayName,
                     created: Date.now(),
                     playlist: []
