a9e8b7f18212da77549e6ca4b341382b15451043
Insoo Choo <earl93@hotmail.com>
2014-10-06 21:44:18 -0400

    qube.js clean up

--- static/javascript/qube.js
+++ static/javascript/qube.js
@@ -1,11 +1,5 @@
 angular.module('QubeApp', [])
   .controller('QubeCont', ['$scope', function($scope, $http) {
-    init();
-    
-    function init(){
-      $scope.hostURL = window.location.protocol + "://" + window.location.host;
-      $scope.playlists = $scope.getPlaylists();
-    }
     
     $scope.getPlaylists = function(){
       $http.get($scope.hostURL + "/listAllPlaylist", {
@@ -19,26 +13,11 @@ angular.module('QubeApp', [])
         alert("input link is broken");
       });
     };
+    
+    function init(){
+      $scope.hostURL = window.location.protocol + "://" + window.location.host;
+      $scope.playlists = $scope.getPlaylists();
+    }
 
-
-    $scope.addTodo = function() {
-      $scope.todos.push({text:$scope.todoText, done:false});
-      $scope.todoText = '';
-    };
- 
-    $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
- 
-    $scope.archive = function() {
-      var oldTodos = $scope.todos;
-      $scope.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.todos.push(todo);
-      });
-    };
+    init();
   }]);
