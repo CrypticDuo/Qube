354287d3fbb0e09bcb664ee9cc2febd518290bbb
Insoo Choo <earl93@hotmail.com>
2014-10-12 22:02:47 -0400

    added instant search for playlists

--- static/javascript/qube.js
+++ static/javascript/qube.js
@@ -11,6 +11,7 @@ app.controller('QubeCont', function($scope, QubeService) {
 
     $scope.addPlaylist = function() {
         QubeService.addPlaylist($scope, $scope.addPlaylistInput);
+        $scope.addPlaylistInput = '';
     }
 });
 
@@ -64,5 +65,35 @@ app.service("QubeService", function($http, $q) {
         addPlaylist: addPlaylist
     });
 
+
+});
+
+app.filter('searchFor', function(){
+
+	// All filters must return a function. The first parameter
+	// is the data that is to be filtered, and the second is an
+	// argument that may be passed with a colon (searchFor:searchString)
+
+	return function(arr, searchString){
+
+		if(!searchString){
+			return arr;
+		}
+
+		var result = [];
+
+		searchString = searchString.toLowerCase();
+
+		// Using the forEach helper method to loop through the array
+		angular.forEach(arr, function(item){
+
+			if(item.name.toLowerCase().indexOf(searchString) !== -1){
+				result.push(item);
+			}
+
+		});
+
+		return result;
+	};
 
 });
