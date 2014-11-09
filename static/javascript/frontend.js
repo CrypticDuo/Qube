0703456998f7d7588914d93b0dbffdea94f4f0d0
Paul <bumj94@gmail.com>
2014-11-08 22:36:37 -0500

    able to seek through video by progress bar

--- static/javascript/frontend.js
+++ static/javascript/frontend.js
@@ -40,17 +40,23 @@ function onPlayerStateChange(event) {
             scope.nextVideo();
         });
     } else if (event.data === YT.PlayerState.PAUSED) {
-        $el = $('ul.controllers li.fa-pause');
-        $el.toggleClass('fa fa-pause');
-        $el.toggleClass('fa fa-play');
+        if(!$('ul.controllers li:nth-child(2)').hasClass('changing')){
+            $el = $('ul.controllers li.fa-pause');
+            $el.toggleClass('fa fa-pause');
+            $el.toggleClass('fa fa-play');
+        }
         pauseTimer();
         timerStartFlag=false;
     } else if (event.data === YT.PlayerState.PLAYING) {
-        $el = $('ul.controllers li.fa-play');
-        $el.toggleClass('fa fa-pause');
-        $el.toggleClass('fa fa-play');
-
-        onTopHeaderChange();
+        if(!$('ul.controllers li:nth-child(2)').hasClass('changing')){
+            $el = $('ul.controllers li.fa-play');
+            $el.toggleClass('fa fa-pause');
+            $el.toggleClass('fa fa-play');
+            onTopHeaderChange();
+        }
+        else{
+            $('ul.controllers li:nth-child(2)').removeClass('changing');
+        }
 
         /*if($('.looper .item.active span.status').hasClass('nextStatus')){
             $('.looper').looper('next');
@@ -116,12 +122,10 @@ function convertSecondsToTime(s) {
 
 var timerStartFlag=false;
 var isPaused=true;
-var maxTime;
 var duration=0;
 
 function startTimer() {
     isPaused=false;
-    maxTime = player.getDuration();
 }
 
 function pauseTimer(){
@@ -133,17 +137,6 @@ $(document).ready(function() {
     $('.looper').looper({});
     $('.looper').looper('next');
     $('.looper').looper('pause');
-    var t = setInterval(function(){
-        if(!isPaused){
-            duration=parseInt((player.getCurrentTime())/(maxTime)*1000)/10;
-            if(duration >= 99){
-                duration = 99.2;
-            }
-            $('.player > .time-container > span.time').text(convertSecondsToTime(player.getCurrentTime()+1));
-            $(".progressBar div.progressLevel").css('width', duration+'%');
-            $(".progressBar span").css('left', duration+'%');
-        }
-    },250);
 
     $("#addPlaylist").leanModal({
         top: 200,
@@ -165,44 +158,38 @@ $(document).ready(function() {
         text = "";
     });
 
-    $(".volumeBar").slider();
+////////////////////////////////////////////////////////////////////////////////
+// VOLUME BAR
+////////////////////////////////////////////////////////////////////////////////
     var $volume = $(".volumeBar");
+    $volume.slider();
     var $volumeElement = $(".volumeBar span");
     $volumeElement.css('left', 'calc(100% - 5px)');
 
-    $(".progressBar").slider();
-    var $progress = $(".progressBar");
-    var $progressElement = $(".progressBar span");
+    var onVolumeClick = false;
 
     $(".volume i").on('click', function() {
         $(this).toggleClass('fa-volume-up');
         $(this).toggleClass('fa-volume-off');
     });
 
-    $progress.on('mousedown', function() {
-        $(document).on('mousemove', function() {
-            if (parseInt($progressElement.css('left')) > 341) {
-                $progressElement.css('left', '99.2%');
-            }
-        });
-    });
     $volume.on('mousedown', function() {
-        var outerWidth = parseInt($('.volumeBar').css('width'));
-        var prev = outerWidth;
+        onVolumeClick= true;
+        var outerWidth = parseInt($volume.css('width'));
         $(document).on('mousemove', function() {
-            if (prev !== parseInt(parseInt($volumeElement.css('left')) / outerWidth * 100)) {
-                prev = parseInt(parseInt($volumeElement.css('left')) / outerWidth * 100);
-                onVolumeChange($volumeElement, prev);
+            if (outerWidth !== parseInt(parseInt($volumeElement.css('left')) / outerWidth * 100)) {
+                var temp = parseInt(parseInt($volumeElement.css('left')) / outerWidth * 100);
+                onVolumeChange($volumeElement, temp);
                 $(".volumeBar div.volumeLevel").css('width', parseInt($volumeElement.css('left')));
-                if (prev == 0) {
+                if (temp == 0) {
                     $(".volume i").removeClass('fa-volume-up');
                     $(".volume i").removeClass('fa-volume-down');
                     $(".volume i").addClass('fa-volume-off');
-                } else if (prev < 50) {
+                } else if (temp < 50) {
                     $(".volume i").removeClass('fa-volume-up');
                     $(".volume i").addClass('fa-volume-down');
                     $(".volume i").removeClass('fa-volume-off');
-                } else if (prev > 50) {
+                } else if (temp > 50) {
                     $(".volume i").addClass('fa-volume-up');
                     $(".volume i").removeClass('fa-volume-down');
                     $(".volume i").removeClass('fa-volume-off');
@@ -211,18 +198,80 @@ $(document).ready(function() {
         });
     });
     $(document).on('mouseup', function() {
-        var outerWidth = parseInt($('.volumeBar').css('width'));
-        var prev = outerWidth;
-        if (prev !== parseInt(parseInt($volumeElement.css('left')) / outerWidth * 100)) {
-            prev = parseInt(parseInt($volumeElement.css('left')) / outerWidth * 100);
-            onVolumeChange($volumeElement, prev);
-            $(".volumeBar div.volumeLevel").css('width', parseInt($volumeElement.css('left')));
+        if(onVolumeClick){
+            onVolumeClick = false;
+            var outerWidth = parseInt($volume.css('width'));
+            if (outerWidth !== parseInt(parseInt($volumeElement.css('left')) / outerWidth * 100)) {
+                var temp = parseInt(parseInt($volumeElement.css('left')) / outerWidth * 100);
+                onVolumeChange($volumeElement, temp);
+                $(".volumeBar div.volumeLevel").css('width', parseInt($volumeElement.css('left')));
+            }
+        }
+    });
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// PROGRESS BAR
+////////////////////////////////////////////////////////////////////////////////
+    var t = setInterval(function(){
+        if(!isPaused){
+            duration=parseInt((player.getCurrentTime())/(getMaxVideoTime())*1000)/10;
+            if(duration >= 99){
+                duration = 99.2;
+            }
+            $('.player > .time-container > span.time').text(convertSecondsToTime(player.getCurrentTime()+1));
+            $(".progressBar div.progressLevel").css('width', duration+'%');
+            $(".progressBar span").css('left', duration+'%');
+        }
+    },250);
+
+    var $progress = $(".progressBar");
+    $progress.slider();
+    var $progressElement = $(".progressBar span");
+
+    var onProgressClick = false;
+
+    $progress.on('mousedown', function() {
+        onProgressClick = true;
+        $('ul.controllers li:nth-child(2)').addClass('changing');
+        player.pauseVideo();
+        var outerWidth = parseInt($volume.css('width'));
+        $(document).on('mousemove', function() {
+            if (parseInt($progressElement.css('left')) > 341) {
+                $progressElement.css('left', '99.2%');
+            }
+            $(".progressBar div.progressLevel").css('width', parseInt($progressElement.css('left')));
+        });
+    });
+    $(document).on('mouseup', function() {
+        if(onProgressClick){
+            onProgressClick = false;
+            var outerWidth = parseInt($progress.css('width'));
+            if (outerWidth !== parseInt(parseInt($progressElement.css('left')) / outerWidth * 100)) {
+                $(".progressBar div.progressLevel").css('width', parseInt($progressElement.css('left')));
+
+                var newTime = parseInt(parseInt($progressElement.css('left')) / outerWidth * getMaxVideoTime()*100)/100;
+                console.log(newTime);
+                player.seekTo(newTime);
+                player.playVideo();
+            }
         }
     });
 
+    function getMaxVideoTime(){
+        return player.getDuration();
+    }
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// RESIZE DIVS
+////////////////////////////////////////////////////////////////////////////////
     $(window).bind("load resize", function() {
         $('.searchResultColumn').height($(this).height() - ($('.topHeader').outerHeight() + $('.youtubeSearchBar').outerHeight()));
         $('.userVideolist').height($(window).height() - ($('.playlistHeader').outerHeight() + $('.player').outerHeight()));
         $('.userPlaylist').height($(this).height() - ($('.topHeader').outerHeight() + $('.youtubeSearchBar').outerHeight() + $('.yourPlaylists').outerHeight() + $('.playlistSearch').outerHeight() + $('.addPlaylist').outerHeight()));
     });
 });
+////////////////////////////////////////////////////////////////////////////////
