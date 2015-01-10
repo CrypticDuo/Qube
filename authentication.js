4c12442160daba87f84e13735fb7be731ea6d619
Paul <bumj94@gmail.com>
2015-01-10 12:33:46 -0500

    cleanup authenticate.js, update github initialization user db

--- authentication.js
+++ authentication.js
@@ -160,6 +160,8 @@ module.exports = passport.use(new GithubStrategy({
                     facebookID: profile._json.id,
                     name: profile.displayName,
                     created: Date.now(),
+                    lastLogin: Date.now(),
+                    loginCount: 1,
                     playlist: defaultPlaylist
                 });
                 user.save(function(err) {
@@ -174,70 +176,3 @@ module.exports = passport.use(new GithubStrategy({
         });
     }
 ));
-
-/*passport.use(new TwitterStrategy({
-        consumerKey: config.twitter.consumerKey,
-        consumerSecret: config.twitter.consumerSecret,
-        callbackURL: config.twitter.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-        console.log(profile.displayName + " has logged in.");
-        User.findOne({
-            oauthID: profile.id
-        }, function(err, user) {
-            if (err) {
-                console.log(err);
-            }
-            if (!err && user != null) {
-                done(null, user);
-            } else {
-                var user = new User({
-                    oauthID: profile.id,
-                    name: profile.displayName,
-                    created: Date.now()
-                });
-                user.save(function(err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("saving user ...");
-                        done(null, user);
-                    };
-                });
-            };
-        });
-    }
-));*/
-/*
-passport.use(new GoogleStrategy({
-        returnURL: config.google.returnURL,
-        realm: config.google.realm
-    },
-    function(accessToken, refreshToken, profile, done) {
-        console.log(profile.displayName + " has logged in.");
-        User.findOne({
-            oauthID: profile.id
-        }, function(err, user) {
-            if (err) {
-                console.log(err);
-            }
-            if (!err && user != null) {
-                done(null, user);
-            } else {
-                var user = new User({
-                    oauthID: profile.id,
-                    name: profile.displayName,
-                    created: Date.now()
-                });
-                user.save(function(err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("saving user ...");
-                        done(null, user);
-                    };
-                });
-            };
-        });
-    }
-));*/
