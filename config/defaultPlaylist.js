9569bcb7293decee8cbd0be86a194ee6d573a4e9
Sumit Balani <sbalani@uwaterloo.ca>
2017-03-08 19:52:18 -0500

    Update default playlists with music (spotify picks)

--- config/defaultPlaylist.js
+++ config/defaultPlaylist.js
@@ -2,40 +2,61 @@
 
 var defaultPlaylist = [
                         {
-                            "name" : "Pop Music",
+                            "name" : "Teen's Choice",
                             "videos" : [
-                                "JGwWNGJdvx8",
                                 "FM7MFYoylVs",
-                                "D5drYkLiLI8",
+                                "JGwWNGJdvx8",
+                                "PMivT7MJ41M",
+                                "fWGD2Cr9StM",
+                                "1ekZEVeXwek",
+                                "9ORWF5RkdO0",
+                                "h--P8HzYZ74",
+                                "XatXy6ZhKZw",
+                                "-zzP29emgpg",
+                                "hRVfCplkKq4",
+                                "dMK_npDG12Q",
                                 "7F37r50VUTQ",
-                                "8gsGhdZDC-0",
-                                "34Na4j8AVgA",
-                                "UqyT8IEBkvY",
-                                "YQHsXMglC9A"
+                                "lNzHARgbCG8",
+                                "W425ePh2aKw",
+                                "J5qWnG5RQTk"
+
                             ]
                         },
                         {
-                            "name" : "Jimmy Fallon",
+                            "name" : "Weekly Buzz",
                             "videos" : [
-                                "2OFKM2G-dE8",
-                                "RYI8KU7PKc0",
-                                "7PzjIXixe24",
-                                "wkSYGykMiwE",
-                                "0Yw-U34X4sc",
-                                "0PRFAh602kU",
-                                "svOElhHUb38"
+                                "gL-WVjvzu34",
+                                "-AfjBifqK_E",
+                                "GKwO8SJ4_uI",
+                                "v7mIK4fqZf0",
+                                "fkRHyYjv3u4",
+                                "IbQ4FXckI7Q",
+                                "5R9SlFF1J-8",
+                                "w2DeJ0q1Wv4",
+                                "vsEeUNtkUhU",
+                                "FaX64o71vGQ"
                             ]
                         },
                         {
-                            "name" : "BuzzFeed",
+                            "name" : "Good Vibes",
                             "videos" : [
-                                "5SSfwEuP6xU",
-                                "YK4ptwHgIJo",
-                                "R-uJviYD2T4",
-                                "1vARrao4HH8",
-                                "w2-FtU2pZes",
-                                "Q8IWkdGLm70",
-                                "n0tAXKkfvxk"
+                                "Fv_lnUTUQYY",
+                                "HoT1cvWDBjE",
+                                "7dqMyh4ILIg",
+                                "QItwYWV-3Qk",
+                                "h4KCIIZ3Z2c",
+                                "hZe5K1DN4ec",
+                                "sN5__shLZ8k",
+                                "D5drYkLiLI8",
+                                "enum2qHUK70",
+                                "isfrigczpiA",
+                                "DTWAavrVNQA",
+                                "RhU9MZ98jxo",
+                                "kfJOSX1tqgU",
+                                "1YRW1QRKTBc",
+                                "dlF1KxtArCg",
+                                "BqaoAQbkeXo",
+                                "nkziIXxfMrg"
                             ]
                         }
                      ];
