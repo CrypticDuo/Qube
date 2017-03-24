534b88223359472d212e9e37082ae81591b2979d
Bumjung Kim <bj6kim@uwaterloo.ca>
2017-03-24 01:17:12 -0400

    implement general feature modal, add trending

--- static/javascript/frontend.js
+++ static/javascript/frontend.js
@@ -694,20 +694,6 @@ $(document).on('click', function(e) {
 ////////////////////////////////////////////////////////////////////////////////
 // NEW FEATURE INFO FUNCTIONALITY
 ////////////////////////////////////////////////////////////////////////////////
-    function showShare() {
-        $('.new-feature .right').show();
-        $('.new-feature .left').hide();
-        $('.new-feature .autogen').hide();
-        $('.new-feature .share').show();
-    }
-
-    function showAutoGen() {
-        $('.new-feature .right').hide();
-        $('.new-feature .left').show();
-        $('.new-feature .autogen').show();
-        $('.new-feature .share').hide();
-    }
-
     function closeNewFeatureModal() {
         var width = $('body').width();
         var height = $(document).height() - $('.bottomContainer').height();
@@ -733,21 +719,49 @@ $(document).on('click', function(e) {
         });
 
         $(document).on('click', '.lcFeatures', function(e) {
-            showShare();
+            var features = $('.new-feature .feature');
+            for(var i = 0; i < features.length; i++) {
+              $(features[i]).hide();
+              $(features[i]).addClass('show');
+            }
+            $(features[0]).show();
+            $('.new-feature .right i').removeClass('off');
             $('.new-feature').css('top', 0);
             $('.new-feature').css('left', 0);
             $('.new-feature').css('width', '100%');
             $('.new-feature').css('height', '100%');
             $('.new-feature').show();
-
         });
 
-        // TODO: make this more dynamic
+        var feature_index = 0;
         $(document).on('click', '.new-feature .left', function(e) {
-            showShare();
+            var features = $('.new-feature .feature.show');
+            if(feature_index === features.length-1 && features.length !== 1) {
+              $('.new-feature .right i').removeClass('off');
+            }
+            if(feature_index-1 === 0) {
+              $('.new-feature .left i').addClass('off');
+            }
+
+            if(feature_index-1 >= 0) {
+              feature_index--;
+              $(features[feature_index+1]).hide();
+              $(features[feature_index]).show();
+            }
         });
         $(document).on('click', '.new-feature .right', function(e) {
-            showAutoGen();
+            var features = $('.new-feature .feature.show');
+            if(feature_index === 0 && features.length !== 1) {
+              $('.new-feature .left i').removeClass('off');
+            }
+            if(feature_index+1 === features.length-1) {
+              $('.new-feature .right i').addClass('off');
+            }
+            if(feature_index+1 < features.length) {
+              feature_index++;
+              $(features[feature_index-1]).hide();
+              $(features[feature_index]).show();
+            }
         });
     }());
 ////////////////////////////////////////////////////////////////////////////////
